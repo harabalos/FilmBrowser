dd662d966ba22ec89705338dc9b9d3d81f8b2794
Harabalos <babismetaxas000@gmail.com>
2022-12-26 13:07:31 +0200

    Added correct film buttons

--- Netflix/FilmBrowser.h
+++ Netflix/FilmBrowser.h
@@ -12,7 +12,12 @@ public:
 protected:
 	static FilmBrowser* m_instance;
 	std::vector<Film*> allFilms;
-	Button filmGenre{ 25.0f, 270.0f, 1.0f, 1.0f, 1.0f, {"Drama"}};
+	Button filmGenre0{ 25.0f, 270.0f, 1.0f, 1.0f, 1.0f, {"Drama"}};
+	Button filmGenre1{ 25.0f, 270.0f, 1.0f, 1.0f, 1.0f, {"Adventure","Action"}};
+	Button filmGenre2{ 25.0f, 270.0f, 1.0f, 1.0f, 1.0f, {"Drama","Crime"}};
+	Button filmGenre3{ 25.0f, 270.0f, 1.0f, 1.0f, 1.0f, {"Adventure","Action","Fantasy"}};
+	Button filmGenre4{ 25.0f, 270.0f, 1.0f, 1.0f, 1.0f, {"Drama","History"}};
+	Button filmGenre5{ 25.0f, 270.0f, 1.0f, 1.0f, 1.0f, {"Sci fi","Action"}};
 	Film* m_active_film = nullptr;
 	FilmBrowser() {};
 		 
